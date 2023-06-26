7b78e4f3cbb1f6834c2152722d65fc8459af730a
姜华清 <jianghq@authine.com>
2023-06-26 15:41:59 +0800

    feat: api页面开发

--- console-ui/src/components/module/api/ListFilter.vue
+++ console-ui/src/components/module/api/ListFilter.vue
@@ -1,5 +1,6 @@
 <script lang="ts" setup>
-import { reactive } from 'vue';
+import { reactive, ref } from 'vue';
+import { apiService } from '@/service';
 const emit = defineEmits(['search']);
 
 const formValue = reactive({
@@ -18,11 +19,47 @@ const onReset = () => {
   formValue.apiUrl = '';
 };
 
+const domainList = ref<Array<{ value: string; label: string; }>>([]);
+const domainLoading = ref(false);
+let domainLoaded = false;
+
+async function onVisibleChange (val: boolean) {
+  if (domainLoaded) {
+    return;
+  }
+  if (val) {
+    domainLoading.value = true; 
+    const res = await apiService.domainQuery({ pageNum: 1, pageSize: 999 });
+    if (res.success) {
+      domainList.value = res.result.map(item => ({ label: item.domainName, value: item.id }));
+    }
+    domainLoaded = true;
+    domainLoading.value = false; 
+  }
+}
+
 </script>
 <template>
   <el-form :inline="true" :model="formValue">
     <el-form-item label="领域">
-      <el-input v-model="formValue.domainId" placeholder="请输入领域名称" />
+      <el-select
+        v-model="formValue.domainId"
+        placeholder="请选择领域"
+        filterable
+        @visibleChange="onVisibleChange"
+      >
+        <template v-slot:empty>
+          <div class="select-option-empty" v-loading="domainLoading">
+            <span v-if="!domainLoading">无数据</span>
+          </div>
+        </template>
+        <el-option
+          v-for="item in domainList"
+          :key="item.value"
+          :label="item.label"
+          :value="item.value"
+        />
+      </el-select>
     </el-form-item>
     <el-form-item label="接口名称">
       <el-input v-model="formValue.apiName" placeholder="请输入接口名称" />
@@ -38,3 +75,10 @@ const onReset = () => {
     </el-form-item>
   </el-form>
 </template>
+<style lang="less">
+.select-option-empty {
+  height: 120px;
+  align-items: center;
+  justify-content: center;
+}
+</style>
