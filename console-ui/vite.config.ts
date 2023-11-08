173cafd2df9c78896d38a2d9ceba776fcf4e0a21
姜华清 <ttshangxiang@qq.com>
2023-11-08 23:53:34 +0800

    chore: 使用.env.local配置apiProxy

--- console-ui/vite.config.ts
+++ console-ui/vite.config.ts
@@ -1,20 +1,21 @@
 import { fileURLToPath, URL } from 'node:url';
-
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import { ApiProxy } from './local.config';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  return {
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
-  server: {
-    proxy: {
-      '^/v(d*)': ApiProxy,
+    server: {
+      proxy: {
+        '^/v(d*)': env.VITE_API_PROXY,
+      },
     },
-  },
+  };
 });
