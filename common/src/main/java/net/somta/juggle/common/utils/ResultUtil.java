25f49442fd2ad674fdb8e865c013e560bca682ce
husong <305123882@qq.com>
2024-07-09 19:40:01 +0800

    fix:调整结果工具类

--- common/src/main/java/net/somta/juggle/common/utils/ResultUtil.java
+++ common/src/main/java/net/somta/juggle/common/utils/ResultUtil.java
@@ -1,5 +1,7 @@
 package net.somta.juggle.common.utils;
 
+import org.apache.commons.collections4.CollectionUtils;
+
 import java.util.*;
 
 public class ResultUtil {
@@ -9,29 +11,23 @@ public class ResultUtil {
         if(containsDot){
             Object value = null;
             String[] params = paramKey.split("\\.");
+            Map<String, Object> temp = new HashMap<>();
             for (int i = 0; i < params.length; i++) {
                 String currentKey = params[i];
-                System.out.println(params[i]);
-                if(i < params.length){
-                    Map<String, Object> temp = (Map<String, Object>) originalResult.get(currentKey);
+                if(i+1 < params.length){
+                    if(temp.isEmpty()){
+                        temp = (Map<String, Object>) originalResult.get(currentKey);
+                    }else {
+                        temp = (Map<String, Object>) temp.get(currentKey);
+                    }
                 }else {
-                    value = originalResult.get(currentKey);
+                    if(temp.isEmpty()){
+                        value = originalResult.get(currentKey);
+                    }else {
+                        value = temp.get(currentKey);
+                    }
                 }
-
             }
-
-            List<String> stringList = Arrays.asList(params);
-
-            // 获取迭代器
-            Iterator<String> iterator = stringList.iterator();
-
-            // 使用迭代器遍历列表
-            while (iterator.hasNext()) {
-                String str = iterator.next();
-                System.out.println(str);
-
-            }
-
             return value;
         } else {
             return originalResult.get(paramKey);
@@ -40,19 +36,29 @@ public class ResultUtil {
 
     public static void main(String[] args) {
         Map<String, Object> nestedMap = new HashMap<>();
-        Map<String, String> innerMap = new HashMap<>();
+
+        Map<String, Object> innerMap = new HashMap<>();
         innerMap.put("key2", "value2");
 
+        Map<String, Object> map = new HashMap<>();
+        map.put("key3", "value3");
+        innerMap.put("key21", map);
+
         nestedMap.put("key1", "value1");
-        nestedMap.put("key3", innerMap);
+        nestedMap.put("map1", innerMap);
+
 
         // 使用JsonPath获取嵌套Map中的值
         //String value = JsonPath.read(nestedMap, "$.key3.key2");
+
         /*Object value1 = getResultFromData("key1",nestedMap);
         System.out.println(value1); // 输出 "value2"*/
 
-        Object value2 = getResultFromData("key3.key2",nestedMap);
-        System.out.println(value2);
+        /*Object value2 = getResultFromData("map1.key2",nestedMap);
+        System.out.println(value2);*/
+
+        Object value3 = getResultFromData("map1.key21.key3",nestedMap);
+        System.out.println(value3);
 
     }
 
