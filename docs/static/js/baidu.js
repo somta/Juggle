4ed47d594790de0a82b677c1bbe421d520b6c9a1
husong <305123882@qq.com>
2024-10-18 12:28:26 +0800

    fix:添加统计

--- docs/static/js/baidu.js
+++ docs/static/js/baidu.js
@@ -3,11 +3,11 @@ var _hmt = _hmt || [];
     var hm = document.createElement("script");
     var origin = window.location.origin;
     if (origin.indexOf("juggle.plus") > -1) {
-        hm.src = "https://hm.baidu.com/hm.js?a423316c57ca2c2fc3a6914fd753692d";
+        hm.src = "https://hm.baidu.com/hm.js?689d52441d8a61e2d50c86dd41503cca";
     } else if (origin.indexOf("www.juggle.plus" > -1)) {
-        hm.src = "https://hm.baidu.com/hm.js?a423316c57ca2c2fc3a6914fd753692d";
+        hm.src = "https://hm.baidu.com/hm.js?689d52441d8a61e2d50c86dd41503cca";
     } else {
-        hm.src = "https://hm.baidu.com/hm.js?a423316c57ca2c2fc3a6914fd753692d";
+        hm.src = "https://hm.baidu.com/hm.js?689d52441d8a61e2d50c86dd41503cca";
     }
     var s = document.getElementsByTagName("script")[0];
     s.parentNode.insertBefore(hm, s);
