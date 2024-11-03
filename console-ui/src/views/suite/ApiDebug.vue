29a66fb4bc75ef50d80bf2f0f7377d1e7bb63001
明天的地平线 <305123882@qq.com>
2024-11-03 23:45:41 +0800

    fix:添加数据类型展示组件

--- console-ui/src/views/suite/ApiDebug.vue
+++ console-ui/src/views/suite/ApiDebug.vue
@@ -8,6 +8,7 @@ import { ApiInfo, DataTypeItem, InputParams } from '@/typings';
 import FilterValue from '@/components/filter/FilterValue.vue';
 import DataTypeSelect from '@/components/form/DataTypeSelect.vue';
 import { InfoFilled } from '@element-plus/icons-vue';
+import DataTypeDisplay from "@/components/common/DataTypeDisplay.vue";
 
 const route = useRoute();
 let paramsData = reactive({
@@ -208,7 +209,8 @@ function resetParams() {
               </el-tooltip>
             </div>
             <div class="input-param-td">
-              <DataTypeSelect :modelValue="param.dataType" disabled />
+              <DataTypeDisplay :dataType="param.dataType"/>
+<!--              <DataTypeSelect :modelValue="param.dataType" disabled />-->
             </div>
             <div class="input-param-td td-value">
               <FilterValue v-model="param.value" :dataType="param.dataType" />
