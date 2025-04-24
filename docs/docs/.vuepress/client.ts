2731bb66e27fb0afab1e75e5548ff3e0eb60a17f
明天的地平线 <305123882@qq.com>
2025-04-24 22:54:52 +0800

    fix: 添加赋值节点的文档

--- docs/docs/.vuepress/client.ts
+++ docs/docs/.vuepress/client.ts
@@ -25,13 +25,12 @@ export default defineClientConfig({
         app.component('Price', VPPrice)
 
         router.beforeEach((to, from, next) => {
-            console.log("切换路由", to.fullPath, from.fullPath);
 
             //触发百度的pv统计
             if (typeof _hmt != "undefined") {
                 if (to.path) {
                     _hmt.push(["_trackPageview", to.fullPath]);
-                    console.log("上报百度统计", to.fullPath);
+                    //console.log("上报百度统计", to.fullPath);
                 }
             }
             // continue
